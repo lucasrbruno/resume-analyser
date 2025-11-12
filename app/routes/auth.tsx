1aed56cf1d1ed0b5019ea65e979fe62cec35ea45
lucasrbruno <lr@mokamind.com.br>
2025-11-12 20:26:58 -0300

    avancei no sistema de login e barra de upload do site

---
+++ app/routes/auth.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter"
+
+export const meta = () => ([
+    { title: 'Analisador de Currículo | Autenticação' },
+    { name: 'description', content: 'Página de autenticação do Analisador de Currículo' }
+])
+
+const auth = () => {
+    const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (auth.isAuthenticated)
+            navigate(next);
+    }, [auth.isAuthenticated, next]);
+
+    return (
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Bem vindo</h1>
+                        <h2>Entre pra continuar sua jornada de trabalho</h2>
+                    </div>
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                <p>Autenticando...</p>
+                            </button>
+                        ) : (
+                            <>
+                                {auth.isAuthenticated ? (
+                                    <button className="auth-button" onClick={auth.signOut}>
+                                        <p>Log Out</p>
+                                    </button>
+                                ) : (
+                                    <button className="auth-button" onClick={auth.signIn}>
+                                        <p>Log In</p>
+                                    </button>
+                                )}
+                            </>
+                        )}
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
+}
+
+export default auth
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,43 @@
+import { useEffect } from "react";
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants/index";
+import ResumeCard from "~/components/ResumeCard";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyser" },
+    { name: "description", content: "Smart AI resumé analyser" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+   const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!auth.isAuthenticated)
+            navigate('/auth?next=/');
+    }, [auth.isAuthenticated]);
+    
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Fique de olho nas suas aplicações e currículos</h1>
+        <h2>Analise seus currículos com IA e aumente suas chances de conseguir o emprego dos seus sonhos!</h2>
+      </div>
+
+       {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
+    </section>
+ 
+  </main>;
 }
